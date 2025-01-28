5a8a9859eebd4b74a6f1314bc981278eb5f1e553
markus <datamastery87@gmail.com>
2025-01-28 20:19:15 +0100

    add code

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,4 @@ sed -i "s/yourdomain.com/$DOMAIN/g" nginx/default.conf.production
 echo "Starting Docker Compose in production mode..."
 docker compose -f docker-compose.production.yaml up -d --build
 
-echo "Requesting certificate with certbot..."
-docker compose -f docker-compose.production.yaml run --rm certbot \
-  certonly --webroot -w /usr/share/nginx/html \
-  -d "$DOMAIN"
-
-echo "Restarting Nginx to load certificate..."
-docker compose -f docker-compose.production.yaml restart nginx
-
 echo "Done! Visit https://$DOMAIN"
